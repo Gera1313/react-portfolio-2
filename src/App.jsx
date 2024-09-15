05793a9511ec105e65ce5c15b8de4e60f3ab801a
Mark Anthony <touya1313@hotmail.com>
2024-09-15 08:07:12 -0600

    resized image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-import '../src/styles'
+import './styles/App.css';
 
 function App() {
   return (
