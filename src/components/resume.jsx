8867810cc883c72a53cdfc7de694228c6d27b083
Mark Anthony <touya1313@hotmail.com>
2024-09-15 06:45:10 -0600

    added placeholder code

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
 
+function Resume() {
+  return (
+    <div>
+      <h1>My Resume</h1>
+      <p>Here’s a link to download my resume and a list of my proficiencies.</p>
+    </div>
+  );
+}
 
 export default Resume;
