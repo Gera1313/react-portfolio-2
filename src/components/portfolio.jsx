6254cc4ec1df2fa7ccbf7f7f41822ecccc2f92b9
Mark Anthony <touya1313@hotmail.com>
2024-09-16 01:06:21 -0600

    adding projects in portfolio

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 function Portfolio() {
+  const projects = [
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+    { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
+  ];
+  
   return (
     <div>
       <h1>Portfolio</h1>
