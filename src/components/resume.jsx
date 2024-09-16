0e0cec79c72ed26a24992d23815e5c6bc2676545
Mark Anthony <touya1313@hotmail.com>
2024-09-16 02:39:56 -0600

    experience

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -24,6 +24,28 @@ function Resume() {
         </ul>
       </div>
 
+            {/* Experience Section */}
+            <div className="experience-section">
+        <h3>Experience</h3>
+        <div className="job">
+          <h4>Junior Web Developer</h4>
+          <p>Company Name | Jan 2023 - Present</p>
+          <ul>
+            <li>Developed and maintained responsive websites using React.js and Node.js.</li>
+            <li>Collaborated with a team of developers to build web applications for client projects.</li>
+            <li>Worked on improving site performance and optimizing components.</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h4>Freelance Web Developer</h4>
+          <p>Self-employed | June 2021 - Dec 2022</p>
+          <ul>
+            <li>Built custom websites for small businesses using modern web technologies.</li>
+            <li>Provided ongoing support and updates for existing client websites.</li>
+          </ul>
+        </div>
+      </div>
+
 
     </section>
   );
