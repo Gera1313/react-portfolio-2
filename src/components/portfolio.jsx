2ea96eb42e934909c697595a038dda7a3856c580
Mark Anthony <touya1313@hotmail.com>
2024-09-16 01:13:26 -0600

    portfolio.jsx

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Project from './Project';
 
 function Portfolio() {
   const projects = [
@@ -9,12 +10,21 @@ function Portfolio() {
     { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
     { id: 1, title: 'Project 1', description: 'description of project here', link: 'https://github.com/Gera1313/Password-Generator' },
   ];
-  
+
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>Here are some of my projects.</p>
-    </div>
+    <section className="portfolio-section">
+      <h2>My Projects</h2>
+      <div className="project-list">
+        {projects.map((project) => (
+          <Project 
+            key={project.id} 
+            title={project.title} 
+            description={project.description} 
+            link={project.link} 
+          />
+        ))}
+      </div>
+    </section>
   );
 }
 
