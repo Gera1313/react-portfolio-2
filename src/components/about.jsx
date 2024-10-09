f55245fab9d6efb08b02cb439acfdccd783a4f84
Mark Anthony <touya1313@hotmail.com>
2024-10-08 18:55:20 -0600

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,7 +17,7 @@ function About() {
             Hello, I am Gerardo Pérez, a passionate web developer based in the USA. I have a strong passion for creating web applications that are not only functional but also visually appealing. With 2 years of experience in web development, I have worked on a wide range of projects.
           </p>
           <p>
-            When I am not coding, I enjoy backpacking in different countries, going to raves and concerts in the city, long treks in nature, camping expeditions to the mountains, and anything adventurous. I am always eager to learn and stay up-to-date with the latest web development trends and technologies. 
+            When I am not coding, I enjoy backpacking in different countries, going to raves and concerts in the city, long treks in nature, camping expeditions to the mountains, video games, anime, and anything adventurous. I am always eager to learn and stay up-to-date with the latest web development trends and technologies. 
           </p>
         </div>
       </section>
